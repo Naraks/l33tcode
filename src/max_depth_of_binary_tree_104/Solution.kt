d65ee418143ffb0ee2ef6592461ac5df9ebaa735
naraks <progenitor123@gmail.com>
2023-08-12 17:11:15 +0700

    add 104

--- src/max_depth_of_binary_tree_104/Solution.kt
+++ src/max_depth_of_binary_tree_104/Solution.kt
@@ -2,8 +2,28 @@ package max_depth_of_binary_tree_104
 
 class Solution {
 
-    fun maxDepth(root: TreeNode?): Int {
+    private var maxDepth  = 0
 
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) return maxDepth;
+
+        calc(0, root)
+
+        return maxDepth
+    }
+
+    private fun calc(depth: Int, node: TreeNode) {
+        val currDepth = depth + 1
+
+        if (maxDepth < currDepth) maxDepth = currDepth
+
+        if (node.left != null) {
+            calc(currDepth, node.left!!)
+        }
+
+        if (node.right != null) {
+            calc(currDepth, node.right!!)
+        }
     }
 
 }
