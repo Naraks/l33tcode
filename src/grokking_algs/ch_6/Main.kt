a27bd16eddb7cc5a5be0e41bf787676d18dc7121
s.rudenko <s.rudenko.sis@gmail.com>
2023-08-18 12:32:28 +0700

    add 6

---
+++ src/grokking_algs/ch_6/Main.kt
@@ -0,0 +1,19 @@
+package grokking_algs.ch_6
+
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sol = Solution()
+
+        val graph: HashMap<String, List<String>> = HashMap()
+        graph.put("You", listOf("Bob", "Claire", "Alice"))
+        graph.put("Claire", listOf("Thom", "Jonny"))
+        graph.put("Alice", listOf("Peggy"))
+        graph.put("Bob", listOf("Peggy", "Anuj"))
+
+        val alg = sol.alg("You", graph) { it == "Peggy" }
+        println(alg)
+    }
+
+}
---
+++ src/grokking_algs/ch_6/Solution.kt
@@ -0,0 +1,27 @@
+package grokking_algs.ch_6
+
+import java.util.LinkedList
+import java.util.function.Predicate
+
+class Solution {
+
+    fun alg(start: String, graph: HashMap<String, List<String>>, predicate: Predicate<String>): String {
+        val queue = LinkedList<String>()
+        queue.addLast(start)
+
+        while (queue.isNotEmpty()) {
+            val current = queue.pollFirst()
+            if (predicate.test(current)) {
+                return current
+            } else {
+                val neighbors = graph[current]
+                if (neighbors != null) {
+                    queue.addAll(neighbors)
+                }
+            }
+        }
+
+        return "nothing!"
+    }
+
+}
