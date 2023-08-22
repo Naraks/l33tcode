f98b08266c0dc5137d12e534108b8944f4fbb7db
s.rudenko <s.rudenko.sis@gmail.com>
2023-08-22 07:47:32 +0700

    add alghs

---
+++ src/algorithms_sedgewick_wayne/Main.kt
@@ -0,0 +1,10 @@
+package algorithms_sedgewick_wayne
+
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+
+}
---
+++ src/algorithms_sedgewick_wayne/readme.md
@@ -0,0 +1,5 @@
+Algorithms
+4 edition
+Robert Sedgewick and Kevin Wayne
+
+algs4.cs.princeton.edu
